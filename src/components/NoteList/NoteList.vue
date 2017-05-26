b08971e83ff86d61229652b2dfa0e04c837ee64a
Mr-Lpy <lpy@lpydeMacBook-Air.local>
2017-05-26 12:39:21 +0800

    优化笔记列表样式

--- src/components/NoteList/NoteList.vue
+++ src/components/NoteList/NoteList.vue
@@ -1,62 +1,64 @@
 <template>
-  <div id="notes-list">
-    <div id="list-header">
-      <h2>易笔记 | LPY</h2>
-      <div class="btn-group btn-group-justified" role="group">
-        <div class="btn-group" role="group">
-          <button type="button" class="btn btn-default" @click="show = 'all'" :class="{active: show==='all'}">
-            全部笔记
-          </button>
+    <div id="notes-list">
+        <div id="list-header">
+            <h2>易笔记 | LPY</h2>
+            <div class="btn-group btn-group-justified" role="group">
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" @click="show = 'all'" :class="{active: show==='all'}">
+                        全部笔记
+                    </button>
+                </div>
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-default" @click="show='favorites'"
+                            :class="{active:show==='favorites'}">我的收藏
+                    </button>
+                </div>
+            </div>
         </div>
-        <div class="btn-group" role="group">
-          <button type="button" class="btn btn-default" @click="show='favorites'" :class="{active:show==='favorites'}">我的收藏</button>
+        <div class="container">
+            <div class="list-group">
+                <transition-group name="el-zoom-in-top" tag="ul" class="trans">
+                    <a v-for="note in filteredNotes" class="list-group-item" href="#"
+                       :class="{active:activeNote === note}" @click="updateActiveNote(note)" :key="note">
+                        <h4 class="list-group-item-heading">{{note.title.trim().substring(0,30)}}</h4>
+                    </a>
+                </transition-group>
+            </div>
         </div>
-      </div>
     </div>
-    <div class="container">
-      <div class="list-group">
-        <transition-group name="el-zoom-in-top" tag="ul">
-          <a v-for="note in filteredNotes" class="list-group-item" href="#" :class="{active:activeNote === note}" @click="updateActiveNote(note)" :key="note">
-            <h4 class="list-group-item-heading">{{note.title.trim().substring(0,30)}}</h4>
-          </a>
-        </transition-group>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
-  export default {
-      data(){
-          return {
-              show: 'all'
-          };
-      },
-      components: {
-          'el-collpase-transition' : CollapseTransition
-      },
 
-    computed: {
-      activeNote() {
-        return this.$store.getters.getActive;
-      },
-      filteredNotes() {
-          if (this.show === 'all') {
-              console.log(this.$store.getters.notes);
-              return this.$store.getters.notes;
-          }
-          return this.$store.getters.notes.filter(note => note.favorite);
-      }
-    },
-    methods: {
-      updateActiveNote(note) {
-          return this.$store.dispatch('updateActiveNote',note);
-      }
+    export default {
+        data(){
+            return {
+                show: 'all'
+            };
+        },
+
+        computed: {
+            activeNote() {
+                return this.$store.getters.getActive;
+            },
+            filteredNotes() {
+                if (this.show === 'all') {
+                    console.log(this.$store.getters.notes);
+                    return this.$store.getters.notes;
+                }
+                return this.$store.getters.notes.filter(note => note.favorite);
+            }
+        },
+        methods: {
+            updateActiveNote(note) {
+                return this.$store.dispatch('updateActiveNote', note);
+            }
+        }
     }
-  }
 </script>
 
 <style>
-
+    .trans {
+        padding: 0;
+    }
 </style>
