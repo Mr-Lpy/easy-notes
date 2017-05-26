0f27b1a3256f13a22c1b9826572ab2183bc3966d
Mr-Lpy <lpy@lpydeMacBook-Air.local>
2017-05-26 14:56:08 +0800

    将主界面改为路由方式显示

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="main">
+        <Toolbar></Toolbar>
+        <note-list></note-list>
+        <Editor></Editor>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import Toolbar from '../Toolbar/Toolbar.vue';
+    import NoteList from '../NoteList/NoteList.vue';
+    import Editor from '../Editor/Editor.vue';
+
+    export default {
+        components: {
+            Toolbar,
+            NoteList,
+            Editor
+        }
+    }
+</script>
+
+<style>
+    .main {
+        height: 100%;
+    }
+</style>
